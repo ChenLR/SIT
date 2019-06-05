5185f18fdf549af49103da8f822b7d9d3cece99d
AnfengAlex <anfengxu136@gmail.com>
2019-06-05 16:42:27 -0700

    v0.15 for 2nd prototype

---
+++ stm32/smart-tile-v0.15/src/protocal.h
@@ -0,0 +1,15 @@
+#ifndef __PROTOCAL_H
+#define __PROTOCAL_H
+
+#include "stm32f10x.h"
+#include <stdio.h>
+
+#define PROT_FIRST_BYTE		0x0D
+#define PROT_SECOND_BYTE	0x0A
+#define PROT_PADD_BYTE		0x50
+#define MAX_PACKAGE_LEN		20
+#define MAX_BUFFER_LEN		41
+
+
+
+#endif
